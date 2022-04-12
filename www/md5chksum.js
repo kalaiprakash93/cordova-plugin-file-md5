19c8bb04ff27fad7a87ecc0d00b9d429f23a6772
kalaiprakash93 <kalaiprakash93@gmail.com>
2022-04-12 17:11:30 +0530

    Update md5chksum.js

--- www/md5chksum.js
+++ www/md5chksum.js
@@ -1,7 +1,7 @@
-exports.file = function(fileEntry, success, error){
+exports.fileCheck = function(fileEntry, success, error){
 		var path = fileEntry.toURL();
 		if (path){
-			cordova.exec(success, error, "md5chksum", "file", [path]);
+			cordova.exec(success, error, "md5chksum", "fileCheck", [path]);
 		}else{
 			error("md5chksum: no path");
 		}
